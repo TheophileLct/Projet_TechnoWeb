ea54e7373a76212726dace7cef15d0c283fd3610
Alex <alexandre.rodier@isen.yncrea.fr>
2019-10-10 16:51:57 +0200

    Update cart.php

---
+++ cart.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="cart.css" />
+    <?php include 'header.php'?>
+    
+</head>
+
+<body>
+
+</body>
+
+<footer>
+<?php include 'footer.php'?>
+</footer>
+
+</html>
