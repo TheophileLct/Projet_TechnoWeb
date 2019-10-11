8f7a402677727c3922f902661baed7e0414517c2
Alex <alexandre.rodier@isen.yncrea.fr>
2019-10-11 16:05:44 +0200

    Size
    Add Size

--- cart.php
+++ cart.php
@@ -13,7 +13,20 @@
         <aside>
         <h1>T-shirt</h1>
         <p>Confectionné en jersey doux, notre version du t-shirt à col rond arbore une coupe ajustée et a été délavé pour un look usé.</p>
-        <div class="size">Size: M <input class="number" type="number" value="1" min="0" max="99"> </div>
+        <div class="size">Size:
+                <FORM>
+                    <SELECT name="size" size="1">
+                        <OPTION>XXL
+                        <OPTION>XL
+                        <OPTION>L
+                        <OPTION selected>M
+                        <OPTION>S
+                        <OPTION>XS
+                        <OPTION>XXS
+                    </SELECT>
+                </FORM>
+            </div> 
+        <div class="quantity">Quantity: <input class="number" type="number" value="1" min="0" max="99"> </div>
         <div class="price"> 50 € </div>
         </aside>
     </section>
@@ -23,7 +36,20 @@
         <aside>
             <h1>Sweat-capuche</h1>
             <p>Ce sweat à capuche à cordon de serrage en molleton dans la palette de couleurs éclatantes de la saison est rehaussé de notre broderie Big Pony distinctive, pour une finition traditionnelle. </p>
-            <div class="size">Size: M <input class="number" type="number" value="1" min="0" max="99"> </div>
+            <div class="size">Size:
+                <FORM>
+                    <SELECT name="size" size="1">
+                        <OPTION>XXL
+                        <OPTION>XL
+                        <OPTION>L
+                        <OPTION selected>M
+                        <OPTION>S
+                        <OPTION>XS
+                        <OPTION>XXS
+                    </SELECT>
+                </FORM>
+            </div> 
+            <div class="quantity">Quantity: <input class="number" type="number" value="1" min="0" max="99"> </div>
             <div class="price"> 135 € </div>
         </aside>
     </section>
@@ -33,8 +59,20 @@
         <aside>
             <h1>Jogging</h1>
             <p> Élégant et sportif grâce à son tissu doux en tricot double, ce pantalon de jogging sera idéal pour vos séances d'entraînement ou pour se détendre le week-end. </p>
-            <div class="size">Size: M <input class="number" type="number" value="1" min="0" max="99"> </div>
-            <input class="number" type="number" value="1" min="0" max="99">
+            <div class="size">Size:
+                <FORM>
+                    <SELECT name="size" size="1">
+                        <OPTION>XXL
+                        <OPTION>XL
+                        <OPTION>L
+                        <OPTION selected>M
+                        <OPTION>S
+                        <OPTION>XS
+                        <OPTION>XXS
+                    </SELECT>
+                </FORM>
+            </div> 
+            <div class="quantity">Quantity: <input class="number" type="number" value="1" min="0" max="99"> </div>
             <div class="price"> 90 € </div>
         </aside>
     </section>
