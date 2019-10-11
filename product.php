01eb78a24e747b507105abf70f32894666b9ac50
TheophileLct <50861637+TheophileLct@users.noreply.github.com>
2019-10-11 15:40:52 +0200

    nouveau produit

--- product.php
+++ product.php
@@ -12,29 +12,29 @@
 
     <div class="section">
         <img id="T-shirt_Double_coton" src="img/T_shirt_1.jpg" alt="Image de notre magnifique T-shirt"/> 
-        <div class="aside">
+        <aside>
             <h1>T-shirt</h1>
             <div class="price"> 50 € </div>
             <p>Confectionné en jersey doux, notre version du t-shirt à col rond arbore une coupe ajustée et a été délavé pour un look usé.</p>
-        </div>
+        </aside>
     </div>
 
     <div class="section">
         <img id="Pull_rapide" src="img/Pull_1.jpg" alt="Image de notre magnifique pull"/>
-        <div class="aside">
+        <aside>
             <h1>Sweat-capuche</h1>
             <div class="price"> 135 € </div>
             <p>Ce sweat à capuche à cordon de serrage en molleton dans la palette de couleurs éclatantes de la saison est rehaussé de notre broderie Big Pony distinctive, pour une finition traditionnelle. </p>
-        </div>
+        </aside>
     </div>
 
     <div class="section">
         <img id="Jogging de procrastination" src="img/jogging_1.jpg" alt="Image de notre magnifique jogging"/>
-        <div class="aside">
+        <aside>
             <h1>Jogging</h1>
             <div class="price"> 90 € </div>
             <p> Élégant et sportif grâce à son tissu doux en tricot double, ce pantalon de jogging sera idéal pour vos séances d'entraînement ou pour se détendre le week-end. </p>
-        </div>
+        </aside>
     </div>
 
     <footer>
