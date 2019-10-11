304bafd5af2b9898f141fbfcb8d11d95a265922e
linakda <40620181+linakda@users.noreply.github.com>
2019-10-11 14:26:59 +0200

    head + foot

--- footer.php
+++ footer.php
@@ -9,6 +9,7 @@
                 <h1>About</h1>
                 <h1>Contact Us</h1>
                 <h1> Privacy Policy</h1>     
+                <a href="search.php" style="text-decoration: none ; color: white"> Recherche</a>
         </div>
 </body>
 </html>
