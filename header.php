d3c51f4f1665f4b92241e2abca5d76598886f03a
linakda <40620181+linakda@users.noreply.github.com>
2019-10-11 16:27:53 +0200

    header

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+    <link rel="icon" type="image/png" href="img/Logo_Diabolo_2.png" />
         <link rel="stylesheet" href="css/header.css" />
     </head>
 
@@ -17,8 +18,6 @@
         </div>
 
        <div id="searchbar">
-               
-               <h1>Que cherchez-vous ?</h1>
                <form action="" class="formulaire">
                <input class="champ" type="text" placeholder="Search..."/>
                     <input class="bouton" type="button" value=" " />
@@ -27,26 +26,20 @@
                 </div>
 
         <div class='panier'>
-                 <!-- <div class='img-panier'>
-                        <img src="img/cart.png" alt="logo" >
-                   </div> -->
-
-                <a href="cart.php" style="text-decoration: none ; color: black"> Panier</a>
+        <a href="cart.php" style="text-decoration: none ; color: black"> Panier</a>
     
             </div>
 
         <div class='compte'>
-                <!-- <div class='img-compte'>
-                        <img src="img/compte.png" alt="logo"  >
-                   </div> -->
-
             <a href="account.php" style="text-decoration: none; color: black"> Compte </a>
 
         </div> 
         </div>
         
         <div class='produit'>
-            <a href="product.php" style="text-decoration: none ; color: white"> Produit </a>
+            <a href="product.php" style="text-decoration: none ; color: white" class=test > Produit </a>
+            <a class="test" >Nos Modèles  </a>
+            <a class=test >  Galerie  </a>
             </div> 
 
        
