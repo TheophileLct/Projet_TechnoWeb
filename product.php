731678c008a44eede68b8813e3369412cc1565c4
Romain Deffrenne <romain.deffrenne@isen.yncrea.fr>
2019-10-16 09:43:12 +0200

    Revert "Update product.css"
    This reverts commit 8dcb2171e78b5cb8348cba9f0f1467b10fe94d96.

--- product.php
+++ product.php
@@ -1,7 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Diabolo</title>
+    <link rel="stylesheet" href="css/product.css" />
+</head>
+<body>
+   
+    <header>
+    <?php include 'header.php'?>
     </header>
 
     <div class="section">
-        <img id="Monica" src="img/Produit_1.jpg" alt="Soutien-gorge avec armatures"/> 
         <img id="Monica" src="img/Produit_1.jpg" alt="Soutien-gorge avec armatures0"/> 
         <aside>
             <h1>Monica - Soutien-gorge avec armatures </h1>
@@ -20,7 +29,9 @@
             <div class="price"> 59 € </div>
             <p>Moderne et féminin, voici l’iconique Soutien-gorge avec armatures, en dentelle 100% française. forme corbeille emboîtant. Dos cheminée et doublure renforcée. Confére un très bon maintien. Convient davantage aux potrines moyennes à généreuses. Disponible jusqu'au bonnet F.</p>
         </aside>
-@ -23,6 +35,18 @@
+    </div>
+
+    <div class="section">
         <img id="Josephine" src="img/Produit_2.jpg" alt="Soutien-gorge avec armatures"/>
         <aside>
             <h1>Josephine - Soutien-gorge avec armatures </h1>
@@ -39,7 +50,9 @@
             <div class="price"> 59 € </div>
             <p>La nouvelle dentelle française. Soutien-gorge avec armatures, en dentelle 100% française, stretch et douce. Motif floral et géométrique délicat. Forme emboitante pour un maintien optimal jusqu'au bonnet F. Modèle sans coques. Excellent maintien. Convient à toutes les poitrines.<p>
         </aside>
-@ -32,6 +56,18 @@
+    </div>
+
+    <div class="section">
         <img id="Chic" src="img/Produit_3.jpg" alt="Soutien-gorge corbeille"/>
         <aside>
             <h1>Chic - Soutien-gorge corbeille</h1>
@@ -58,3 +71,12 @@
             <div class="price"> 69 € </div>
             <p> La dentelle Leavers Soutien-gorge corbeille, en dentelle française, strech et douce. Festons de dentelle sophistiqués. Bonnets doublés pour un maintien renforcé. Modèle avec armatures, sans coques. Bon maintien. Convient à toutes les poitrines.</p>
         </aside>
+    </div>
+
+    <footer>
+    <?php include 'footer.php'?>
+    </footer>
+</body>
+
+
+</html>
