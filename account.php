58028d3edb1e803c7c9ca08298c2743011bf4771
Romain Deffrenne <romain.deffrenne@isen.yncrea.fr>
2019-10-17 17:41:58 +0200

    Update account.php

--- account.php
+++ account.php
@@ -10,12 +10,12 @@
 
 <body>
     <center>
- 	  <form id="login">
+ 	  <form id="login" action="connected.php" method="post">
         <h1>Log In</h1>
         <fieldset id="inputs">
-            <input id="username" type="text" placeholder="E-mail" autofocus required>  
+            <input name="username1"  id="username" type="text" placeholder="Username" autofocus required>  
             <h1></h1>
-            <input id="password" type="password" placeholder="Mot de Passe" autofocus required>
+            <input name="password1" id="password" type="password" placeholder="Mot de Passe" autofocus required>
         </fieldset>
         <fieldset id="actions">
             <input type="submit" id="submit" value="Se connecter">
@@ -27,11 +27,11 @@
     <form id="create" action="conf_form.php" method="post">
         <h2>Create</h2>
         <fieldset id="inputs">
-         <input id="username" type="text" placeholder="Username" autofocus required> 
+         <input  name="username" id="username" type="text" placeholder="Username" autofocus required> 
             <h2></h2>
-            <input id="email" type="text" placeholder="E-mail" autofocus required> 
+            <input name='email'id="email" type="text" placeholder="E-mail" autofocus required> 
             <h2></h2>
-            <input id="password" type="password" placeholder="Mot de passe" autofocus required>
+            <input name='password'id="password" type="password" placeholder="Mot de passe" autofocus required>
             <h2></h2>
             <input id="password" type="password" placeholder="Vérifier votre mot de passe" size="22"autofocus required>
         </fieldset>
