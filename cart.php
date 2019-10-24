b16e562294dfb61af129c1937d20f86c4ac38a37
Alex <alexandre.rodier@isen.yncrea.fr>
2019-10-24 19:23:29 +0200

    Ajout des infos du produits dans des listes
    On a une liste de noms de produits, une liste de description de produit, une liste de quantité de produits et une liste de prix de produits

--- cart.php
+++ cart.php
@@ -9,22 +9,43 @@
         session_start ();
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $NOM = $_SESSION['login'];
-        print($NOM);
         $req = $conn->prepare('SELECT id FROM users WHERE username ="'.$NOM.'"');
         $req->execute();
         $donnees = $req->fetch();
-        if($donnees){
-            print($donnees[0]);
-        }else{
-            print("PAS OK");
-        }
         $req1 = $conn->prepare('SELECT name, description, prod.unit_price, ord_prod.quantity FROM products prod, order_products ord_prod, orders ord WHERE (ord.type = "CART" AND ord_prod.order_id = ord.id AND prod.id = ord_prod.product_id AND ord.user_id ="'.$donnees[0].'")');
         $req1->execute();
         $donnees1 = $req1->fetch();
         if ($donnees1) {
+            $k = 0;
+            $Name = [];
+            $Description = [];
+            $Price =  [];
+            $Quantity = [];
             foreach($donnees1 as $elem){
-                print($elem);
-                print(" ");
+                if($k<sizeof($donnees1)/4){
+                    array_push($Name, $elem);
+                }else{
+                    if($k<2*sizeof($donnees1)/4){
+                        array_push($Description, $elem);
+                    }else{
+                        if($k<3*sizeof($donnees1)/4){
+                            array_push($Price, $elem);
+                        }else{
+                            array_push($Quantity, $elem);
+                        }
+                    }
+                }
+                $k++;
+            }
+            for($k=0;$k<sizeof($Name);$k++){
+                echo $Name[$k];
+                echo "  ";
+                echo $Description[$k];
+                echo "  ";
+                echo $Price[$k];
+                echo "  ";
+                echo $Quantity[$k];
+                echo "  ";
             }
         }else{
             print("PAS OK");
@@ -69,50 +90,6 @@
                 </div> 
             <div class="quantity">Quantity: <input class="number" type="number" value="1" min="0" max="99"> </div>
             <div class="price"> 59 € </div>
-            </aside>
-        </section>
-
-        <section>
-            <img id="Josephine" src="img/Produit_2.jpg" alt="Soutien-gorge avec armatures"/>
-            <aside>
-                <h1>Josephine - Soutien-gorge avec armatures </h1>
-                <p>La nouvelle dentelle française. Soutien-gorge avec armatures, en dentelle 100% française, stretch et douce. Motif floral et géométrique délicat. Forme emboitante pour un maintien optimal jusqu'au bonnet F. Modèle sans coques. Excellent maintien. Convient à toutes les poitrines.<p>
-                <div class="size">Size:
-                    <FORM>
-                        <SELECT name="size" size="1">
-                            <OPTION>F
-                            <OPTION>E
-                            <OPTION>D
-                            <OPTION selected>C
-                            <OPTION>B
-                            <OPTION>A
-                        </SELECT>
-                    </FORM>
-                </div> 
-                <div class="quantity">Quantity: <input class="number" type="number" value="1" min="0" max="99"> </div>
-                <div class="price"> 59 € </div>
-            </aside>
-        </section>
-
-        <section>
-            <img id="Chic" src="img/Produit_3.jpg" alt="Soutien-gorge corbeille"/>
-            <aside>
-                <h1>Chic - Soutien-gorge corbeille</h1>
-                <p> La dentelle Leavers Soutien-gorge corbeille, en dentelle française, strech et douce. Festons de dentelle sophistiqués. Bonnets doublés pour un maintien renforcé. Modèle avec armatures, sans coques. Bon maintien. Convient à toutes les poitrines.</p>
-                <div class="size">Size:
-                    <FORM>
-                        <SELECT name="size" size="1">
-                            <OPTION>F
-                            <OPTION>E
-                            <OPTION>D
-                            <OPTION selected>C
-                            <OPTION>B
-                            <OPTION>A
-                        </SELECT>
-                    </FORM>
-                </div> 
-                <div class="quantity">Quantity: <input class="number" type="number" value="1" min="0" max="99"> </div>
-                <div class="price"> 69 € </div>
             </aside>
         </section>
 
