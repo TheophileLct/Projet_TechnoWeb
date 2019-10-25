6efac32e4e6b1c9c82eb6d50073b868d27cae2a4
linakda <40620181+linakda@users.noreply.github.com>
2019-10-25 16:31:07 +0200

    product dyn
    produit en dynamique

--- product.php
+++ product.php
@@ -1,64 +1,49 @@
+
+<?php 
+
+ $servername = "localhost";
+ $username = "root";
+ $password = "root";
+ $dbname = "bddprojet";
+
+ try {
+    session_start ();
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+ $req1 = $conn->prepare('SELECT name, description, unit_price FROM products WHERE 1');
+ $req1->execute();
+ }
+
+ catch(PDOException $e){
+    echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 <head>
     <title>Diabolo</title>
     <link rel="stylesheet" href="css/product.css" />
 </head>
+
 <body>
    
     <header>
     <?php include 'header.php'?>
     </header>
 
-    <div class="section">
-        <img id="Monica" src="img/Produit_1.jpg" alt="Soutien-gorge avec armatures0"/> 
-        <aside>
-            <h1><a href="product_1.php" style="text-decoration: none ; color: black"> Monica - Soutien-gorge avec armatures </a></h1>
-            <div class="size">Taille:
-                <FORM>
-                    <SELECT name="size" size="1">
-                        <OPTION>90B
-                        <OPTION>90C
-                        <OPTION>90D
-                        <OPTION selected>90E
-                        <OPTION>90F
-                    </SELECT>
-                </FORM>
-            </div> 
-            <form action="cart_save.php" method="post">
-        <div class="quantity"><input type="submit" id="submit" value="Ajouter au panier"> </div>
-            <div class="price"> 59 € </div>
-            <p>Moderne et féminin, voici l’iconique Soutien-gorge avec armatures, en dentelle 100% française. forme corbeille emboîtant. Dos cheminée et doublure renforcée. Confére un très bon maintien. Convient davantage aux potrines moyennes à généreuses. Disponible jusqu'au bonnet F.</p>
-            
-        </form>
-        </aside>
-    </div>
-
-    <div class="section">
-        <img id="Josephine" src="img/Produit_2.jpg" alt="Soutien-gorge avec armatures"/>
-        <aside>
-            <h1><a href="product_2.php" style="text-decoration: none ; color: black"> Joséphine - Soutien-gorge avec armatures </a></h1>
-            <div class="size">Taille:
-                <FORM>
-                    <SELECT name="size" size="1">
-                        <OPTION>90B
-                        <OPTION>90C
-                        <OPTION>90D
-                        <OPTION selected>90E
-                        <OPTION>90F
-                    </SELECT>
-                </FORM>
-            </div> 
-            <div class="price"> 59 € </div>
-            <div class="quantity"><input type="submit" id="submit" value="Ajouter au panier"> </div>
-            <p>La nouvelle dentelle française. Soutien-gorge avec armatures, en dentelle 100% française, stretch et douce. Motif floral et géométrique délicat. Forme emboitante pour un maintien optimal jusqu'au bonnet F. Modèle sans coques. Excellent maintien. Convient à toutes les poitrines.<p>
-        </aside>
-    </div>
+    <?php 
+            while ($data = $req1->fetch()) {
+        ?>
 
     <div class="section">
         <img id="Chic" src="img/Produit_3.jpg" alt="Soutien-gorge corbeille"/>
         <aside>
-            <h1><a href="product_3.php" style="text-decoration: none ; color: black"> Chic - Soutien-gorge corbeille </a></h1>
+            <h1><a href="product_3.php" style="text-decoration: none ; color: black"> <?php echo $data[0]?> </a></h1>
             <div class="size">Taille:
                 <FORM>
                     <SELECT name="size" size="1">
@@ -70,11 +55,12 @@
                     </SELECT>
                 </FORM>
             </div> 
-            <div class="price"> 69 € </div>
+            <div class="price"> <?php echo $data[2]?>€ </div>
             <div class="quantity"><input type="submit" id="submit" value="Ajouter au panier"> </div>
-            <p> La dentelle Leavers Soutien-gorge corbeille, en dentelle française, strech et douce. Festons de dentelle sophistiqués. Bonnets doublés pour un maintien renforcé. Modèle avec armatures, sans coques. Bon maintien. Convient à toutes les poitrines.</p>
+            <p> <?php echo $data[1]?></p>
         </aside>
     </div>
+    <?php } ?>
 
     <footer>
     <?php include 'footer.php'?>
