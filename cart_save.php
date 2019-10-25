36987320d221e4d8086a1da7cfd3012d73fcd3dd
Delannoy <louisyonnaled@gmail.com>
2019-10-25 14:22:46 +0200

    myaccount changes

---
+++ cart_save.php
@@ -0,0 +1,31 @@
+<?php
+
+$id=$_POST[""];
+
+
+echo 'vous avez selectionné ';
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bddprojet";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO order_products (quantity, )
+    VALUES (1, )";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
+
+
+?>
