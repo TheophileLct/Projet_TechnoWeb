247dabd71c2895d3102907760b233c2a003f6e12
TheophileLct <50861637+TheophileLct@users.noreply.github.com>
2019-10-25 16:25:50 +0200

    search en cours

--- bdd.php
+++ bdd.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $dbname = "bddprojet";
 
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
