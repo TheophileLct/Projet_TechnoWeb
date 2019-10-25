b80abd49b15e34f8aa7d9f3e3385f1096cc75be4
TheophileLct <50861637+TheophileLct@users.noreply.github.com>
2019-10-25 14:23:54 +0200

    Nouveau footer

--- footer.php
+++ footer.php
@@ -7,9 +7,12 @@
 
     <body>
         <div class = "pied" >
+                </br>
+                </br>
+                </br>
                 <h4><a href="about.php" style="text-decoration: none ; color: pink">A propos</h4>
-                <h4>Contact us</h4>   
-                <h4> <a href="search.php" style="text-decoration: none ; color: pink">Recherche</a> </h4>
+                <h4><a href="contact.php" style="text-decoration: none; color: pink">Contactez-nous</h4>   
+                <h4><a href="search.php" style="text-decoration: none ; color: pink">Recherche</a> </h4>
         </div>
 </body>
 </html>
