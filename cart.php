e733d69e6298e67076e0f38e74974e1b6128e664
Alex <alexandre.rodier@isen.yncrea.fr>
2019-10-25 16:07:34 +0200

    Affichage Dynamique Cart

--- cart.php
+++ cart.php
@@ -19,29 +19,7 @@
         $req2 = $conn->prepare('SELECT SUM(products.unit_price*order_products.quantity) FROM products, order_products, orders WHERE (orders.type = "CART" AND order_products.order_id = orders.id AND products.id = order_products.product_id AND orders.user_id = ?)');
         $req2->execute(Array($donnees[0]));
         $donnees2 = $req2->fetch();
-        echo $donnees2[0];
-
-        $Name = [];
-        $Description = [];
-        $Price =  [];
-        $Quantity = [];
-        while($donnees1 = $req1->fetch()){
-                array_push($Name, $donnees1[0]);
-                array_push($Description, $donnees1[1]);
-                array_push($Price, $donnees1[2]);
-                array_push($Quantity, $donnees1[3]);
-            }
-            /*for($k=0;$k<sizeof($Name);$k++){
-                echo $Name[$k];
-                echo "  ";
-                echo $Description[$k];
-                echo "  ";
-                echo $Price[$k];
-                echo "  ";
-                echo $Quantity[$k];
-                echo "  ";
-            }*/
-        }
+    }
 
     catch(PDOException $e){
         echo $sql . "<br>" . $e->getMessage();
@@ -62,32 +40,37 @@
         <header>
         <?php include 'header.php'?>
         </header>
-        <section>
-            <img id="Monica" src="img/Produit_1.jpg" alt="Soutien-gorge avec armatures"/> 
-            <aside>
-            <h1>Monica - Soutien-gorge avec armatures </h1>
-            <p>Moderne et féminin, voici l’iconique Soutien-gorge avec armatures, en dentelle 100% française. forme corbeille emboîtant. Dos cheminée et doublure renforcée. Confére un très bon maintien. Convient davantage aux potrines moyennes à généreuses. Disponible jusqu'au bonnet F.</p>
-            <div class="size">Size:
-                    <FORM>
-                        <SELECT name="size" size="1">
-                            <OPTION>F
-                            <OPTION>E
-                            <OPTION>D
-                            <OPTION selected>C
-                            <OPTION>B
-                            <OPTION>A
-                        </SELECT>
-                    </FORM>
-                </div> 
-            <div class="quantity">Quantity: <input class="number" type="number" value="1" min="0" max="99"> </div>
-            <div class="price"> 59 € </div>
-            </aside>
-        </section>
+
+        <?php 
+            while ($data = $req1->fetch()) {
+        ?>
+            <section>
+                <img id="Monica" src="img/Produit_1.jpg" alt="Soutien-gorge avec armatures"/> 
+                <aside>
+                <h1><?php echo $data[0] ?></h1>
+                <p><?php echo $data[1] ?></p>
+                <div class="size">Size:
+                        <FORM>
+                            <SELECT name="size" size="1">
+                                <OPTION>F
+                                <OPTION>E
+                                <OPTION>D
+                                <OPTION selected>C
+                                <OPTION>B
+                                <OPTION>A
+                            </SELECT>
+                        </FORM>
+                    </div> 
+                <div class="quantity">Quantity: <input class="number" type="number" value=<?php echo $data[3] ?> min="0" max="99"> </div>
+                <div class="price"> <?php echo $data[2]?>€ </div>
+                </aside>
+            </section>
+            <?php } ?>
 
         <section>
             <h1>Votre Commande:</h1>
             <h2><p>Vous avez acheté 3 produits pour un total de:</p></h2>
-            <div class="price_final"> 200 € </div>
+            <div class="price_final"> <?php echo $donnees2[0]; ?>€ </div>
             <input class="bouton_1" type="button" value="Valider" />
         </section>
 
