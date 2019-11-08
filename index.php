f04e7ea7077d7ecc72809c26697fb9eacd9bf3fc
Romain Deffrenne <romain.deffrenne@isen.yncrea.fr>
2019-11-08 14:08:02 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+if (){
+	header ('location: about.php');
+}
+
+if (){
+	header ('location: account.php');
+}
+
+if (){
+	header ('location: cart.php');
+}
+
+if (){
+	header ('location: cart_save.php');
+}
+
+if (){
+	header ('location: conf_form.php');
+}
+
+if (){
+	header ('location: connected.php');
+}
+
+if (){
+	header ('location: contact.php');
+}
+
+if (){
+	header ('location: logout.php');
+}
+
+if (){
+	header ('location: myaccount.php');
+}
+
+if (){
+	header ('location: myaccount_update.php');
+}
+
+if (){
+	header ('location: product.php');
+}
+
+if (){
+	header ('location: product_1.php');
+}
+
+if (){
+	header ('location: product_2.php');
+}
+
+if (){
+	header ('location: product_3.php');
+}
+
+if (){
+	header ('location: research.php');
+}
+
+if (){
+	header ('location: search.php');
+}
+
+?>
