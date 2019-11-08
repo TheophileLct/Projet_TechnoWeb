63389a25cd88000a0f2fca6b9b2c6d986317ae21
linakda <40620181+linakda@users.noreply.github.com>
2019-11-08 14:21:00 +0100

    Footer Linou

--- footer.php
+++ footer.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html>
 
-<<<<<<< HEAD
-    <body>
-        <div class = "pied" >
-                </br>
-                <h4><a href="about.php" style="text-decoration: none ; color: pink">A propos</h4>
-                <h4><a href="contact.php" style="text-decoration: none; color: pink">Contactez-nous</h4>   
-                <h4><a href="researchpage.php" style="text-decoration: none ; color: pink">Recherche</a> </h4>
-        </div>
-=======
 <head>
     <link rel="stylesheet" href="css/footer.css" />
     <title>Diabolo</title>
@@ -20,9 +11,9 @@
         </br>
         <h4><a href="about.php" style="text-decoration: none ; color: pink">About us</h4>
         <h4><a href="contact.php" style="text-decoration: none; color: pink">Contact us</h4>
-        <h4><a href="search.php" style="text-decoration: none ; color: pink">Research</a> </h4>
+        <h4><a href="research.php" style="text-decoration: none ; color: pink">Research</a> </h4>
     </div>
->>>>>>> f04e7ea7077d7ecc72809c26697fb9eacd9bf3fc
+
 </body>
 
 </html>
