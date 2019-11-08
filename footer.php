30918348fd788d7155759a898db6ebb4ccb1c6e3
linakda <40620181+linakda@users.noreply.github.com>
2019-11-08 14:06:47 +0100

    Barre de recherche
    Celle du footer est fonctionnelle

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
                 </br>
                 <h4><a href="about.php" style="text-decoration: none ; color: pink">A propos</h4>
                 <h4><a href="contact.php" style="text-decoration: none; color: pink">Contactez-nous</h4>   
-                <h4><a href="search.php" style="text-decoration: none ; color: pink">Recherche</a> </h4>
+                <h4><a href="researchpage.php" style="text-decoration: none ; color: pink">Recherche</a> </h4>
         </div>
 </body>
 </html>
---
+++ researchpage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" href="css/search.css" />
+    <?php include 'header.php'?>
+    <title>Diabolo</title>
+</head>
+
+<body>
+
+        <div id="searchbar">
+               <form action="research.php" class="formulaire" method="post">
+               <input name=research class="champ" type="text" placeholder="Search..."/>
+                    <input type="submit" id="submit" value="Search" />
+                    
+                </form>
+        </div>
+
+ </body>
+
+<footer> <?php include 'footer.php'?></footer>
+</html>
