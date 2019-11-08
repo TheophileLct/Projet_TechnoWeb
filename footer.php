7b5171db84122484f35f46931164e689f6632a8f
linakda <40620181+linakda@users.noreply.github.com>
2019-11-08 14:36:04 +0100

    researchpage

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
         </br>
         <h4><a href="about.php" style="text-decoration: none ; color: pink">About us</h4>
         <h4><a href="contact.php" style="text-decoration: none; color: pink">Contact us</h4>
-        <h4><a href="research.php" style="text-decoration: none ; color: pink">Research</a> </h4>
+        <h4><a href="researchpage.php" style="text-decoration: none ; color: pink">Research</a> </h4>
     </div>
 
 </body>
