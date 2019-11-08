f8b9663cf651c857555437d4ebf5d5ccd46296f2
linakda <40620181+linakda@users.noreply.github.com>
2019-11-08 14:15:08 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="css/footer.css" />
-        <title>Diabolo</title>
-    </head>
 
+<<<<<<< HEAD
     <body>
         <div class = "pied" >
                 </br>
@@ -12,5 +9,20 @@
                 <h4><a href="contact.php" style="text-decoration: none; color: pink">Contactez-nous</h4>   
                 <h4><a href="researchpage.php" style="text-decoration: none ; color: pink">Recherche</a> </h4>
         </div>
+=======
+<head>
+    <link rel="stylesheet" href="css/footer.css" />
+    <title>Diabolo</title>
+</head>
+
+<body>
+    <div class="pied">
+        </br>
+        <h4><a href="about.php" style="text-decoration: none ; color: pink">About us</h4>
+        <h4><a href="contact.php" style="text-decoration: none; color: pink">Contact us</h4>
+        <h4><a href="search.php" style="text-decoration: none ; color: pink">Research</a> </h4>
+    </div>
+>>>>>>> f04e7ea7077d7ecc72809c26697fb9eacd9bf3fc
 </body>
+
 </html>
