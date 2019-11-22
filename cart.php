371ca37a26da3a3b35bc44e9399da11cfc1045cc
Alex <alexandre.rodier@isen.yncrea.fr>
2019-11-22 17:06:21 +0100

    Bug

--- cart.php
+++ cart.php
@@ -2,13 +2,16 @@
 
     try {
     
+        var_dump($_SESSION);
         $NOM = $_SESSION['login'];
-        $req = $conn->prepare('SELECT id FROM users WHERE username = ?');
+        var_dump($_SESSION);
+        $req = $conn->prepare('SELECT id FROM users WHERE username = '.$NOM.'');
         $req->execute(Array($NOM));
         $donnees = $req->fetch();
 
-        if(isset($_POST['id_produit'])){
-            $id=$_POST['id_produit'];
+
+        if(isset($_POST['id'])){
+            $id=$_POST['id'];
 
             if(isset($_POST['quantity'])){
                 $quantity=$_POST['quantity'];
@@ -16,9 +19,10 @@
                 try {
                     include "bdd.php";
                     if($quantity == 0){
-                        $req4 = $conn->prepare('DELETE FROM order_products, orders WHERE (order_products.product_id = '.$id_produit.' AND order_products.order_id = orders.id AND orders.user_id = ?)');
+                        $req4 = $conn->prepare('DELETE FROM order_products, orders WHERE (order_products.product_id = '.$id.' AND order_products.order_id = orders.id AND orders.user_id = '.$_SESSION['login'].')');
                     }else{
-                        $req4 = $conn->prepare('UPDATE order_products FROM order_products, orders SET quantity = '.$quantity.' WHERE product_id = '.$id_produit.' AND order_products.order_id = orders.id AND orders.user_id = ?)');
+                        $req4 = $conn->prepare('UPDATE order_products FROM order_products, orders SET quantity = '.$quantity.' WHERE product_id = '.$id.' AND order_products.order_id = orders.id AND orders.user_id = '.$_SESSION['login'].')');
+                        echo ('UPDATE order_products FROM order_products, orders SET quantity = '.$quantity.' WHERE product_id = '.$id.' AND order_products.order_id = orders.id AND orders.user_id = '.$_SESSION['login'].')');
                     }                    
                     $req4->execute();
                 }
@@ -28,14 +32,14 @@
             }
         }
 
-        $req1 = $conn->prepare('SELECT name, description, products.unit_price, order_products.quantity, products.id FROM products, order_products, orders WHERE (orders.type = "CART" AND order_products.order_id = orders.id AND products.id = order_products.product_id AND orders.user_id = ?)');
+        $req1 = $conn->prepare('SELECT name, description, products.unit_price, order_products.quantity, products.id FROM products, order_products, orders WHERE (orders.type = "CART" AND order_products.order_id = orders.id AND products.id = order_products.product_id AND orders.user_id = '.$_SESSION['login'].')');
         $req1->execute(Array($donnees[0]));
 
-    $req2 = $conn->prepare('SELECT SUM(products.unit_price*order_products.quantity) FROM products, order_products, orders WHERE (orders.type = "CART" AND order_products.order_id = orders.id AND products.id = order_products.product_id AND orders.user_id = ?)');
+    $req2 = $conn->prepare('SELECT SUM(products.unit_price*order_products.quantity) FROM products, order_products, orders WHERE (orders.type = "CART" AND order_products.order_id = orders.id AND products.id = order_products.product_id AND orders.user_id = '.$_SESSION['login'].')');
     $req2->execute(array($donnees[0]));
     $Price_Total = $req2->fetch();
 
-    $req3 = $conn->prepare('SELECT SUM(order_products.quantity) FROM products, order_products, orders WHERE (orders.type = "CART" AND order_products.order_id = orders.id AND products.id = order_products.product_id AND orders.user_id = ?)');
+    $req3 = $conn->prepare('SELECT SUM(order_products.quantity) FROM products, order_products, orders WHERE (orders.type = "CART" AND order_products.order_id = orders.id AND products.id = order_products.product_id AND orders.user_id = '.$_SESSION['login'].')');
     $req3->execute(array($donnees[0]));
     $Quantity = $req3->fetch();
 } catch (PDOException $e) {
@@ -81,7 +85,7 @@ $conn = null;
                 <div class="price"> <?php echo $data["unit_price"]?>€ </div>
                 <form action='' method='post'>
                     <div class="quantity">Quantity: <input class="number" type="number" name="quantity" value=<?php echo $data["quantity"] ?> min="0" max="99"> </div>
-                    <input type="hidden" name="id_produit" value=<?php echo $data["id"] ?>>
+                    <input type="hidden" name="id" value=<?php echo $data["id"] ?>>
                     <input class="bouton_1" type="submit" value="Valider"/>
                     <p><?php echo $data["description"] ?></p>
                 </form>
